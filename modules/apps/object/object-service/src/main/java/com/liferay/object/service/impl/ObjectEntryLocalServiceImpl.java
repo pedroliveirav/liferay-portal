1019090a480544342728d28e3294fcce10a36181
Rafael Praxedes <rafael.praxedes@liferay.com>
2022-08-18 17:28:50 -0300

    LPS-159589 Clear the dslQuery cache when deleting an objectEntry

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -77,6 +77,7 @@ import com.liferay.portal.kernel.dao.db.DB;
 import com.liferay.portal.kernel.dao.db.DBManagerUtil;
 import com.liferay.portal.kernel.dao.db.DBType;
 import com.liferay.portal.kernel.dao.jdbc.CurrentConnection;
+import com.liferay.portal.kernel.dao.orm.FinderCacheUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.json.JSONArray;
@@ -1282,6 +1283,8 @@ public class ObjectEntryLocalServiceImpl
 			StringBundler.concat(
 				"delete from ", dbTableName, " where ",
 				pkObjectFieldDBColumnName, " = ", primaryKey));
+
+		FinderCacheUtil.clearDSLQueryCache(dbTableName);
 	}
 
 	private Predicate _fillAccountEntriesPredicate(
